bdcadb9090272ae470bdee4f427185f517d0f296
Saulo Medeiros de araujo <saulo.araujo@bcb.gov.br>
2016-05-18 11:07:17 -0300

    Response body verification

--- src/main/java/com/sauloaraujo/stubbymockmvctests/StubbyMockMvc.java
+++ src/main/java/com/sauloaraujo/stubbymockmvctests/StubbyMockMvc.java
@@ -1,5 +1,11 @@
 package com.sauloaraujo.stubbymockmvctests;
 
+import static org.hamcrest.collection.IsIterableWithSize.iterableWithSize;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
@@ -8,12 +14,13 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.commons.io.IOUtils;
+import org.hamcrest.FeatureMatcher;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.JsonPathResultMatchers;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -21,6 +28,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.sauloaraujo.stubbymockmvctests.Specification.Request;
+import com.sauloaraujo.stubbymockmvctests.Specification.Response;
 
 public class StubbyMockMvc {    
     private List<Specification> read(String path) throws JsonParseException, JsonMappingException, IOException {
@@ -29,13 +37,77 @@ public class StubbyMockMvc {
     	CollectionType type = mapper.getTypeFactory().constructCollectionType(Collection.class, Specification.class); 
 		return mapper.readValue(file, type);
     }
+    
+    private <K, V> FeatureMatcher<Map<? extends K, ? extends V>, Integer> mapWithSize(int size) {
+        return new FeatureMatcher<Map<? extends K, ? extends V>, Integer>(equalTo(size), "a map with size", "size of the map") {
+            @Override
+            protected Integer featureValueOf(Map<? extends K, ? extends V> actual) {
+                return actual.size();
+            }
+        };
+    }    
 
-    private void execute(MockMvc mockMvc, Specification specification) throws Exception {
+    private static final String URL_PREFIX = "^";
+    private static final String URL_SUFFIX = "$";
+
+    private void verify(ResultActions actions, String path, Object expected) throws Exception {
+        JsonPathResultMatchers actual = jsonPath(path);
+        actions.andExpect(actual.exists());
+
+        if (expected == null) {
+            actions.andExpect(actual.value(null));
+        } else {
+            if (expected instanceof Boolean) {
+                actions.andExpect(actual.isBoolean());
+                actions.andExpect(actual.value(expected));
+            } else if (expected instanceof Number) {
+                actions.andExpect(actual.isNumber());
+                actions.andExpect(actual.value(expected));
+            } else if (expected instanceof String) {
+                actions.andExpect(actual.isString());
+                actions.andExpect(actual.value(expected));
+            } else if (expected instanceof Map) {
+                actions.andExpect(actual.isMap());
+
+                Map<?, ?> expectedMap = (Map<?, ?>) expected;
+                actions.andExpect(actual.value(mapWithSize(expectedMap.size())));
+
+                for (Entry<?, ?> entry : expectedMap.entrySet()) {
+                    String key = (String) entry.getKey();
+                    Object value = entry.getValue();
+                    verify(actions, path + "." + key, value);                    
+                }
+            } else if (expected instanceof List) {
+                actions.andExpect(actual.isArray());
+
+                List<?> expectedList = (List<?>) expected;
+                actions.andExpect(actual.value(iterableWithSize(expectedList.size())));
+
+                int index = 0;
+                
+                for (Object expectedElement : expectedList) {
+                    verify(actions, path + "[" + index + "]", expectedElement);
+                    ++index;
+                }
+            }
+        }
+    }
+
+    private void execute(MockMvc mockMvc, String urlPrefix, Specification specification) throws Exception {
     	MockHttpServletRequestBuilder builder = null;
 
     	Request request = specification.getRequest();
-    	
+
     	String url = request.getUrl();
+    	if (!url.startsWith(URL_PREFIX)) {
+    	    throw new RuntimeException("URL does not start with the prefix " + URL_PREFIX + ": " + url);
+    	}
+        if (!url.endsWith("$")) {
+            throw new RuntimeException("URL does not end with the suffix " + URL_SUFFIX + ": " + url);
+        }
+        url = url.substring(0, url.length() - URL_SUFFIX.length());
+        url = url.substring(URL_PREFIX.length());
+        url = urlPrefix + url;
     	
     	String method = request.getMethod();
     	if ("DELETE".equals(method)) {
@@ -65,9 +137,9 @@ public class StubbyMockMvc {
     		}
     	}
 
-    	Map<String, String> headers = request.getHeaders();
+    	Map<String, String> requestHeaders = request.getHeaders();
     	if (query != null) {
-    		for (Entry<String, String> entry : headers.entrySet()) {
+    		for (Entry<String, String> entry : requestHeaders.entrySet()) {
     			String name = entry.getKey();
     			String value = entry.getValue();
     			builder.header(name, value);
@@ -81,22 +153,37 @@ public class StubbyMockMvc {
 
     	String file = request.getFile();
     	if (file != null) {
-    		byte[] content = IOUtils.toByteArray(new File(file).toURI()); 
+    	    byte[] content = IOUtils.toByteArray(new File(file).toURI()); 
     		builder.content(content);
     	}
 
-    	ResultActions actions = mockMvc.perform(builder).andDo(MockMvcResultHandlers.print());
+    	ResultActions actions = mockMvc.perform(builder);    	
+    	actions.andDo(MockMvcResultHandlers.print());
     	
-    	Integer status = specification.getResponse().getStatus();
+    	Response response = specification.getResponse(); 
+
+    	Integer status = response.getStatus();
     	if (status != null) {
-    	    actions.andExpect(MockMvcResultMatchers.status().is(status));
+    	    actions.andExpect(status().is(status));
     	}
+
+    	Map<String, String> responseHeaders = response.getHeaders();
+    	if (responseHeaders != null) {
+    	    for (Entry<String, String> header : responseHeaders.entrySet()) {
+    	        String name = header.getKey();
+    	        String value = header.getValue();
+    	        actions.andExpect(header().string(name, value));
+    	    }
+    	}
+
+    	Object body = response.getBody();
+    	verify(actions, "$", body);
     }
 
-    public void execute(MockMvc mockMvc, String specificationsPath) throws Exception {
+    public void execute(MockMvc mockMvc, String urlPrefix, String specificationsPath) throws Exception {
     	List<Specification> specifications = read(specificationsPath);    	
 		for (Specification specification : specifications) {
-			execute(mockMvc, specification);
+			execute(mockMvc, urlPrefix, specification);
 		}    	
     }    
 }
--- src/test/java/com/sauloaraujo/stubbymockmvctests/ProjectControllerTest.java
+++ src/test/java/com/sauloaraujo/stubbymockmvctests/ProjectControllerTest.java
@@ -1,12 +1,27 @@
 package com.sauloaraujo.stubbymockmvctests;
 
+import static java.util.Arrays.asList;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.hateoas.RelProvider;
+import org.springframework.hateoas.core.DefaultRelProvider;
+import org.springframework.hateoas.hal.Jackson2HalModule;
+import org.springframework.hateoas.hal.Jackson2HalModule.HalHandlerInstantiator;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.cfg.HandlerInstantiator;
 
 public class ProjectControllerTest {
     private @Mock ProjectService service;
@@ -19,11 +34,30 @@ public class ProjectControllerTest {
     public void initialize() {
         MockitoAnnotations.initMocks(this);
         
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        Module module = new Jackson2HalModule();        
+
+        RelProvider provider = new DefaultRelProvider();        
+        HandlerInstantiator instantiator = new HalHandlerInstantiator(provider, null, null);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(module);
+        mapper.setHandlerInstantiator(instantiator);
+
+        Charset charset = Charset.forName("UTF-8");
+        MediaType type = new MediaType("application", "hal+json", charset);
+        List<MediaType> types = asList(type);
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();        
+        converter.setObjectMapper(mapper);
+        converter.setSupportedMediaTypes(types);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).setMessageConverters(converter).build();
     }
+    
+    private static final String URL_PREFIX = "http://localhost:8080";
     
 	@Test
 	public void search() throws Exception {    	    
-		new StubbyMockMvc().execute(mockMvc,  "src/test/resources/search.yaml");
+		new StubbyMockMvc().execute(mockMvc,  URL_PREFIX, "src/test/resources/search.yaml");
 	}
 }
